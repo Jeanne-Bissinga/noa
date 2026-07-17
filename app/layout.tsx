79650ad2851dd6ead0c76aa2b5758918c7fe14c5
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2026-07-17 12:10:57 +0200

    tag : add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -43,7 +44,15 @@ export default function RootLayout({
       <body className="antialiased">
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
-        
+        {process.env.NODE_ENV === 'production' && (
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xnsq82o77k");`}
+          </Script>
+        )}
       </body>
     </html>
   )
